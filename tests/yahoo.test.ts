0e151556191560e5af282fe68113b394924725af
JoshForner <fornerjos@gmail.com>
2023-10-08 01:48:37 -0400

    update test

--- tests/yahoo.test.ts
+++ tests/yahoo.test.ts
@@ -2,18 +2,27 @@
 // @ts-ignore
 import { expect, test } from "bun:test";
 import { YahooSports } from "../src/yahoo";
-import * as fs from "fs";
+import { promises as fs } from 'fs';
 
-test.todo("Refresh Token", async () => {
+test("Refresh Token", async () => {
 	const config = {
-		key: process.env.YAHOO_CLIENT_KEY || '',
-		secret: process.env.YAHOO_CLIENT_SECRET || '',
-		authorizationCode: process.env.YAHOO_AUTHORIZATION_CODE || '',
+		key: process.env.YAHOO_CLIENT_KEY2 || '',
+		secret: process.env.YAHOO_CLIENT_SECRET2 || '',
+		authorizationCode: process.env.YAHOO_AUTHORIZATION_CODE2 || '',
 	};
 
-	const yahoo = new YahooSports(config, './tests/token.json');
+	const yahoo = new YahooSports(config, './temp/temp.token.json');
+
+	// await yahoo.getToken();
 
 	await yahoo.refreshToken();
 
-	expect(JSON.parse(fs.readFileSync('token.json').toString())).toHaveProperty('access_token');
+	const tokenData = JSON.parse(await fs.readFile('./temp/temp.token.json', 'utf8'));
+
+	expect(tokenData).toHaveProperty('access_token');
+	expect(tokenData).toHaveProperty('refresh_token');
+	expect(tokenData).toHaveProperty('expires_in');
+	expect(tokenData).toHaveProperty('token_type');
+	expect(tokenData.expires_in).toBe(3600);
+	expect(tokenData.token_type).toBe('bearer');
 });
