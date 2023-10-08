c5302f800d079f10f85da17692b558b7fdfb20e4
JoshForner <fornerjos@gmail.com>
2023-10-08 10:05:51 -0400

    update tests

--- tests/yahoo.test.ts
+++ tests/yahoo.test.ts
@@ -4,6 +4,27 @@ import { expect, test } from "bun:test";
 import { YahooSports } from "../src/yahoo";
 import { promises as fs } from 'fs';
 
+test("Get Token", async () => {
+	const config = {
+		key: process.env.YAHOO_CLIENT_KEY || '',
+		secret: process.env.YAHOO_CLIENT_SECRET || '',
+		authorizationCode: process.env.YAHOO_AUTHORIZATION_CODE || '',
+	};
+
+	const yahoo = new YahooSports(config, './temp/test.token.json');
+
+	await yahoo.getToken();
+
+	const tokenData = JSON.parse(await fs.readFile('./temp/test.token.json', 'utf8'));
+
+	expect(tokenData).toHaveProperty('access_token');
+	expect(tokenData).toHaveProperty('refresh_token');
+	expect(tokenData).toHaveProperty('expires_in');
+	expect(tokenData).toHaveProperty('token_type');
+	expect(tokenData.expires_in).toBe(3600);
+	expect(tokenData.token_type).toBe('bearer');
+});
+
 test("Refresh Token", async () => {
 	const config = {
 		key: process.env.YAHOO_CLIENT_KEY2 || '',
@@ -11,13 +32,11 @@ test("Refresh Token", async () => {
 		authorizationCode: process.env.YAHOO_AUTHORIZATION_CODE2 || '',
 	};
 
-	const yahoo = new YahooSports(config, './temp/temp.token.json');
-
-	// await yahoo.getToken();
+	const yahoo = new YahooSports(config, './temp/test.token.json');
 
 	await yahoo.refreshToken();
 
-	const tokenData = JSON.parse(await fs.readFile('./temp/temp.token.json', 'utf8'));
+	const tokenData = JSON.parse(await fs.readFile('./temp/test.token.json', 'utf8'));
 
 	expect(tokenData).toHaveProperty('access_token');
 	expect(tokenData).toHaveProperty('refresh_token');
